5f87fd10f092284ebb75f72ac9a178f24560bd03
Kelly Hondula <khondula@asurite.asu.edu>
2022-12-01 12:37:57 -0700

    cleanup

--- plots-list.R
+++
@@ -1,9 +0,0 @@
-# RGB
-
-# RGB in context with full image
-
-# install.packages('gridGraphics')
-library(cowplot)
-
-# need to save individual layers that will get used
-cowplot::plot_grid(make_rgb, make_rgb)
